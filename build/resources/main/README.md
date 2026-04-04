964bdbb3ef53c7be547d76317206285d9036617e
kingtrapinch <159817831+kingtrapinch@users.noreply.github.com>
2026-04-04 15:19:23 -0400

    the modification process... has begun...

---
+++ build/resources/main/README.md
@@ -0,0 +1,68 @@
+# Twilight-Forms
+A Minecraft Cobblemon datapack that adds 50+ custom Twilight Forest forms and over 250+ spawns to the Twilight Forest dimension. Requires the Cobblemon and Twilight Forest mods.
+
+Official documentation: https://docs.google.com/spreadsheets/d/1AafuQTbn8ZOYbnxcc8pOjfavEZGB1dysjJn2TxD4Bww/edit?gid=1915767084#gid=1915767084
+
+
+Use in-game command   '/pokedex grant all twilight'   to view all the new Pokemon added to the Twilight Forest dimension
+
+Discord thread post: https://discord.com/channels/934267676354834442/1221575822759170058
+
+Modrinth page: https://modrinth.com/datapack/twilight-forest-forms-cobblemon
+
+For suggestions, post on the discord thread here: https://discord.com/channels/934267676354834442/1195373518095921233
+
+Link to Github repo: https://github.com/kingtrapinch/Twilight-Forms
+
+List of Twilight Forms:
+
+Added in 4.0: 
+- Zubat and Golbat (Ice / Flying)
+- Crobat (Ice / Dark)
+- Darumaka line (Ice, Unovan/Galarian hybrid)
+- Honedge (Grass/Ghost)
+- Doublade (Ice/Ghost)
+- Aegislash (Fire/Steel)
+- Shellos and Gastrodon (Fire / Ground)
+- Hatenna line (Ice / Psychic)
+  
+
+Added in 3.0: 
+
+- Tangela line (Grass / Steel)
+- Drifloon line ( Ghost / Fire)
+- Pinsir (Bug / Dark)
+- Heracross (Bug / Fire)
+- Deino line (Dragon / Fire)
+- Twilight Vivillon form
+
+Added in 2.0: 
+
+- Cubone and Marowak (Grass / Ghost)
+- Necromarrow (Marowak evo, Psychic / Ghost)
+- Aron line (Grass / Steel)
+- Nickit line (Ghost/Dark)
+- Fidough line (Ghost/Normal)
+
+(Removed as of 4.0)
+
+<s>Shadow Forms:
+- Bulbasaur line (Dark/Grass)
+- Charmander line (Dark/Fire)
+- Squirtle line (Dark/Water)
+- Mewtwo (Dark/Psychic)</s>
+  
+Added in 1.0: 
+
+- Torterra (Canopy, Twilight Oak, Fireswamp, and Darkwood)
+- Ekans & Arbok (Poison/Steel)
+- Paras & Parasect (Bug/Poison w/ Water Bubble)
+- Ralts, Kirlia, Gallade, and Gardevoir (Ghost/Fight and Ghost/Fairy)
+- Tropius (Grass/Dragon)
+- Venipede, Whirlipede, Scolipede (Psychic/Dark)
+- Dwebble & Crustle (Underbrick & Mazestone)
+- Deerling & Sawsbuck (Grass/Psychic)
+- Golett & Golurk (Grass/Electric)
+- Wooloo & Dubwool (Fairy/Fight)
+
+To report bugs or suggest improvements to models, visit the github repo's issue page: https://github.com/kingtrapinch/Twilight-Forms/issues
---
+++ src/main/java/net/kingtrapinch/twilightforms/TwilightForms.java
@@ -0,0 +1,83 @@
+package net.kingtrapinch.twilightforms;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraft.client.Minecraft;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(TwilightForms.MODID)
+public class TwilightForms {
+    public static final String MODID = "twilightforms";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public TwilightForms(IEventBus modEventBus, ModContainer modContainer) {
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+
+    }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @EventBusSubscriber(modid = TwilightForms.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    static class ClientModEvents {
+        @SubscribeEvent
+        static void onClientSetup(FMLClientSetupEvent event) {
+
+        }
+    }
+}
